9aad84b105e599b7a8163f7ce09751b00c9ec736
Marlon Menjivar <marlon.menjivar_@hotmail.com>
2020-04-25 18:53:08 -0600

    Adding Regression scripts

---
+++ Regresion-Lessons/Polynomial-Regression/poly_reg.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+
+# Assign the data to predictor and outcome variables
+train_data = pd.read_csv("data.csv")
+X = train_data["Var_X"].values.reshape(-1, 1)
+y = train_data["Var_Y"].values
+
+# Create polynomial features
+# predictor feature
+poly_feat = PolynomialFeatures(degree=4)
+X_poly = poly_feat.fit_transform(X)
+
+# Make and fit the polynomial regression model
+# features
+poly_model = None
+poly_model = LinearRegression(fit_intercept = False).fit(X_poly, y)
+# Once you've completed all of the steps, select Test Run to see your model
+# predictions against the data, or select Submit Answer to check if the degree
+# of the polynomial features is the same as ours!
